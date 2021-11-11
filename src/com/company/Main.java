e4a09515bc110dac1f1717885580a42ec08ce254
feyzkivanc <feyzakivanc@gmail.com>
2021-11-11 15:24:45 +0300

    Password Application

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+        /*Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun,
+        eğer kullanıcı sıfırlamak isterse yeni girdiği şifrenin hatalı girdiği ve unuttuğu şifre
+        ile aynı olmaması gerektiğini kontrol edip , şifreler aynı ise ekrana "Şifre oluşturulamadı, " +
+        "lütfen başka şifre giriniz." sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.*/
 
     public static void main(String[] args) {
-	// write your code here
+        String pass = "3edp67d";
+        Scanner scan = new Scanner(System.in);
+        System.out.print("please enter your password: ");
+        String pass1 = scan.nextLine();
+        if (pass.equals(pass1)) {
+            System.out.println("Login success");
+        } else {
+            System.out.print("Password is not correct. Do you want change your password? yes or no: ");
+            String answer = scan.nextLine();
+            if (answer.equals("yes")) {
+                System.out.print("enter new password: ");
+                String newPass = scan.nextLine();
+                if (newPass.equals(pass)) {
+                    System.out.print("Please enter a another password:");
+                } else {
+                    System.out.print("Password created. Thanks!");
+                }
+
+            } else {
+                System.out.println("Have a nice day!");
+            }
+        }
     }
 }
